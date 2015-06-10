50b6398023cef4931790492593bc2ca61293cf21
inzscript <inzscript@gmail.com>
2015-06-10 02:06:13 +0000

    Added user detection

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,21 @@
 class UsersController < ApplicationController
   
+  before_action :authenticate_user!, only: [:index]
   before_action :set_user, only: [:show]
   
+  # read url parameters: /users?people=pending
   def index
-    @users = User.all
+    case params[:people] 
+      when "friends"
+        @users = current_user.active_friends
+      when "requests"
+        @users = current_user.pending_friend_requests_from.map(&:user)
+      when "pending"
+        @users = current_user.pending_friend_requests_to.map(&:friend)
+      else
+        #display all people except yourself
+        @users = User.where.not(id: current_user.id)
+    end
   end
   
   def show
