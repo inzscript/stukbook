7ab7c58dddfa153047c676071fb20413cec108e6
inzscript <inzscript@gmail.com>
2015-06-10 00:09:02 +0000

    Friendship actions now possible

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,42 @@
+class FriendshipsController < ApplicationController
+
+    # Ensure user is logged in
+    before_action :authenticate_user!
+    before_action :set_user, only: [:create]
+    before_action :set_friendship, only: [:destroy, :accept]
+        
+	# Action to create a freindship request
+	def create
+		@friendship = current_user.request_friendship(@user)
+		respond_to do |format|
+			format.html {redirect_to users_path, notice: "Friendship Created"}
+		end
+	end
+	
+	#Action to remove a friendship request and redirect to People page
+	def destroy
+	    @friendship.destroy
+	    respond_to do |format|
+			format.html {redirect_to users_path, notice: "Friendship Deleted"}
+		end
+	end
+	
+	# Action to Accept friendship
+	def accept
+		@friendship.accept_friendship
+		respond_to do |format|
+			format.html {redirect_to users_path, notice: "Friendship Accepted"}
+		end
+	end
+	
+	private
+	
+	def set_user
+	    @user = User.find(params[:user_id])
+	end
+	
+	def set_friendship
+	    @friendship = Friendship.find(params[:id])
+	end
+
+end
