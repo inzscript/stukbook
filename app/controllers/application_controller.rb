7cd8edf977bdfc3168e01e01c44aa7b5d124d531
inzscript <inzscript@gmail.com>
2015-06-20 22:19:00 +0000

    Final touches before production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
+  
+  helper_method :request_count
+  
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  # Gload variable for friend request count.
+  def request_count
+    current_user.pending_friend_requests_from.map(&:user).size
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   
   before_action :authenticate_user!, only: [:index]
   before_action :set_user, only: [:show]
+  before_action :get_counts, only: [:index]
   
   # read url parameters: /users?people=pending
   def index
@@ -28,6 +29,12 @@ class UsersController < ApplicationController
   end
   
   private
+  
+  # Populate the friend and pending counts
+  def get_counts
+    @friend_count = current_user.active_friends.size
+    @pending_count = current_user.pending_friend_requests_to.map(&:friend).size
+  end
   
   def set_user
     @user = User.find_by(username: params[:id])
